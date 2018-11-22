04125c34b68f96f636d61dbf5e2af51026b3c348
何须回头 <416400304@qq.com>
2018-11-22 11:34:59 +0800

    springboot 定时调度 （最好使用专门的调度平台）

---
+++ src/main/java/com/windows3/scheduled/ScheduledTasks.java
@@ -0,0 +1,23 @@
+package com.windows3.scheduled;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by 3 on 2018/11/22.
+ * 任务调度
+ * 缺点：1.没有报错处理机制
+ *       2.不好管理
+ */
+
+@Component
+public class ScheduledTasks {
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+    @Scheduled(fixedRate = 1000)
+    public void reportCurrentTime() {
+        System.out.println("现在时间：" + dateFormat.format(new Date()));
+    }
+}
